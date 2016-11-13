efe0c4e26b24e38ae167e792fc87ba57d03cec14
Ishan Subedi <ikushum@gmail.com>
2016-11-13 08:21:23 +0000

    fixed some searching bugs

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -12,15 +12,25 @@ $(function(){
     });
   });
 
-  function displayMovies(data){
-    let htmlString = "";
 
-    data["Search"].forEach(function(movie){
-      htmlString += `<img src=${movie["Poster"]} />
-                     <p>${movie["Title"]}</p>
-                     <p>${movie["Year"]}</p>`;
-    });
-
-    $("#movies").append(htmlString);
-  }
+    function displayMovies(data){
+      let container = $("#movies")
+      let htmlString = "";
+    
+      container.empty();
+    
+      if (data["Response"] == "False") {
+        htmlString = `<div class="alert alert-danger text-center" role="alert">${data["Error"]}</div>`
+      } 
+      else {
+    
+        data["Search"].forEach(function(movie){
+          htmlString += `<img src=${movie["Poster"] == "N/A" ? "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRnINMwmB4xbH1-A01_6yCHuugXwvee3rv9fZnuE7EITlJ5tLDR0w" : movie["Poster"]} />
+                         <p>${movie["Title"]}</p>
+                         <p>${movie["Year"]}</p>`;
+        });
+      }
+    
+      container.append(htmlString);
+    }
 });
