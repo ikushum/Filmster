1d01774e2439c0354f2f5ad857510ce95006c76d
Ishan Subedi <ikushum@gmail.com>
2016-11-18 13:13:25 +0000

    built a functional user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root 'staticpages#index'
   resources :reviews
   resources :movies
+  resources :users, only: :show
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
