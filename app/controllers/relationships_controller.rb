fd25e1d318fb68df68b6a84c0de9a48749b9ba2e
Ishan Subedi <ikushum@gmail.com>
2016-11-22 16:34:07 +0000

    displayed followers and following

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,12 +3,10 @@ class RelationshipsController < ApplicationController
   before_action :check_user
 
   def create
-    @user = User.find(params[:user_id])
     current_user.follow(@user)
   end
   
   def destroy
-    @user = User.find(params[:user_id])
     current_user.unfollow(@user)
  end
   
